4556b6a48b2ed093ba2a597fae5d2987bdc0e7fe
vtPapini <victorcostelletos@hotmail.com>
2023-01-22 22:58:16 -0300

    initial CRUDs

--- DB_/CRUDs.py
+++ DB_/CRUDs.py
@@ -13,7 +13,8 @@ def creat_user(phone:int,name:str):
 
     con.commit()
     con.close()
-    
+
+
 def creat_wallet(phone:int,wallet_name:str,):
     con = lite.connect('DB_\DB_.db')
     cur = con.cursor()
@@ -26,23 +27,23 @@ def creat_wallet(phone:int,wallet_name:str,):
     con.commit()
     con.close()
 
-def add_stocks(wallet_name:str,phone:int,stocks_code:str):
-    codes = stocks_code.split('/')
-    print(codes)
+def add_stocks(wallet_ID:int,stocks_code:str,weights:str):
+    code_stock = stocks_code.split('/')
+    code_weight = weights.split('/')
+    
     con = lite.connect('DB_\DB_.db')
     cur = con.cursor()
 
-    for index in codes:
+    for index1,index2 in zip(code_stock,code_weight) :
         cur.execute(
-                    """UPDATE TB_WALLET SET STOCK_CODE = '{}' 
-                    WHERE PHONE_NUM = {} 
-                    AND Wallet_name = '{}';""".format(index,phone,wallet_name)
+                    """INSERT INTO TB_CODE (WALLET_ID,STOCK_CODE,WEIGHT) 
+                   VALUES ({},'{}',{});""".format(wallet_ID,index1,float(index2))
                     )
+
     con.commit()
     con.close()
 
 
-
 #creat_user(13,'Victor')
-creat_wallet(13,'Fundo Varejo')
-#add_stocks('Fundo imobiliário',13,'yyyy/KKKK')
+#creat_wallet(13,'Fundo Imobiliário')
+#add_stocks(3,'fff/ççç','0.50/0.50')
